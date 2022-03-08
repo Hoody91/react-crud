66c160c631f8ef906201d579d41fededacc367c5
Riley Hood <riley.hood18@gmail.com>
2022-03-08 19:52:10 +1100

    basic looking form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>CRUD Application</h1>
+      <div className="form">
+        <label>Framework Name:</label>
+        <input type="text" name="frameworkName" />
+        <label>Framework Review:</label>
+        <input type="text" name="frameworkReview" />
+        <button>Submit</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
